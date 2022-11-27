ccd81cca652be70aa7e58660a06c28363f215030
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-27 00:15:40 -0300

    feat: home route

--- src/page/pagehome/home.js
+++ src/page/pagehome/home.js
@@ -1,9 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ItemContainer, ItemsContainer } from "../../styled";
 
-export default function Home(){
-    return(
-        <>
-        Liste seus produtos.
-        </>
-    )
+const URL = "http://localhost:5000/home";
+let itens = [];
+
+export default function Home() {
+  const [itemsArray, setItemsArray] = useState([]);
+  const [selectedItemsArray, setSelectedItemsArray] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((data) => {
+        setItemsArray(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function selecionaItem(param) {
+    if (!selectedItemsArray.includes(param)) {
+      itens = [...selectedItemsArray, param];
+      setSelectedItemsArray(itens);
+    } else {
+      const newItens = itens.filter((item) => item != param);
+      setSelectedItemsArray(newItens);
+    }
+  }
+
+  console.log(selectedItemsArray);
+
+  return (
+    <ItemsContainer>
+      {itemsArray.length > 0
+        ? itemsArray.map((item, index) => {
+            return (
+              <ItemContainer
+                onClick={() => selecionaItem(item._id)}
+                key={index}
+              >
+                <img src={item.image} />
+                <h1>
+                  {item.name} R${parseFloat(item.price).toFixed(2)}
+                </h1>
+              </ItemContainer>
+            );
+          })
+        : ""}
+    </ItemsContainer>
+  );
 }
--- src/styled.js
+++ src/styled.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
     background-color: darkorange;
     font-family: Open-Sans, Helvetica, Sans-Serif;
   }
-`
+`;
 
 export const ContainerCadastro = styled.div`
   width: 100%;
@@ -17,23 +17,23 @@ export const ContainerCadastro = styled.div`
   align-items: center;
   justify-content: center;
 
-  h1{
-    font-family: 'Saira Stencil One', cursive;
+  h1 {
+    font-family: "Saira Stencil One", cursive;
     font-style: normal;
     font-weight: 400;
     font-size: 32px;
     line-height: 50px;
-    color: #FFFFFF;
+    color: #ffffff;
   }
-  p{
-    font-family: 'Raleway', sans-serif;
+  p {
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 700;
     font-size: 15px;
     line-height: 18px;
-    color: #FFFFFF;
+    color: #ffffff;
   }
-`
+`;
 
 export const FormUp = styled.form`
   width: 100%;
@@ -41,38 +41,38 @@ export const FormUp = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  input{
+  input {
     width: 326px;
     height: 58px;
-    background: #FFFFFF;
+    background: #ffffff;
     border-radius: 5px;
     margin: 5px;
     padding: 5px;
 
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 400;
     font-size: 20px;
     line-height: 23px;
     color: #000000;
-    border: 1px solid #FFFFFF;
+    border: 1px solid #ffffff;
   }
-  button{
+  button {
     width: 326px;
     height: 46px;
     background: darkolivegreen;
     border-radius: 5px;
     margin: 5px;
-    cursor:pointer;
+    cursor: pointer;
 
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 700;
     font-size: 20px;
-    color: #FFFFFF;
+    color: #ffffff;
     border: 1px solid darkorange;
   }
-`
+`;
 
 export const ContainerLogin = styled.div`
   width: 100%;
@@ -82,23 +82,23 @@ export const ContainerLogin = styled.div`
   align-items: center;
   justify-content: center;
 
-  h1{
-    font-family: 'Saira Stencil One', cursive;
+  h1 {
+    font-family: "Saira Stencil One", cursive;
     font-style: normal;
     font-weight: 400;
     font-size: 32px;
     line-height: 50px;
-    color: #FFFFFF;
+    color: #ffffff;
   }
-  p{
-    font-family: 'Raleway', sans-serif;
+  p {
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 700;
     font-size: 15px;
     line-height: 18px;
-    color: #FFFFFF;
+    color: #ffffff;
   }
-`
+`;
 
 export const FormIn = styled.form`
   width: 100%;
@@ -106,35 +106,71 @@ export const FormIn = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  input{
+  input {
     width: 326px;
     height: 58px;
-    background: #FFFFFF;
+    background: #ffffff;
     border-radius: 5px;
     margin: 5px;
     padding: 5px;
 
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 400;
     font-size: 20px;
     line-height: 23px;
     color: #000000;
-    border: 1px solid #FFFFFF;
+    border: 1px solid #ffffff;
   }
-  button{
+  button {
     width: 326px;
     height: 46px;
     background: darkolivegreen;
     border-radius: 5px;
     margin: 5px;
-    cursor:pointer;
+    cursor: pointer;
 
-    font-family: 'Raleway', sans-serif;
+    font-family: "Raleway", sans-serif;
     font-style: normal;
     font-weight: 700;
     font-size: 20px;
-    color: #FFFFFF;
+    color: #ffffff;
     border: 1px solid darkorange;
   }
-`
+`;
+
+export const ItemsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 20px;
+`;
+
+export const ItemContainer = styled.div`
+  background-color: gray;
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  h1 {
+    font-size: 15px;
+    font-weight: bold;
+  }
+  img {
+    border-radius: 10px;
+    width: 250px;
+    height: 250px;
+  }
+  @media (max-width: 768px) {
+    width: 130px;
+    img {
+      width: 150px;
+      height: 150px;
+    }
+  }
+`;
